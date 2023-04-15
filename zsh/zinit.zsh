99697498ac9253ed02b09bc76b2b564d054b498e
octkmr <octkmr@gmail.com>
2023-04-15 14:41:38 +0900

    オートコンプリート追加

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -3,3 +3,5 @@ zinit ice pick"async.zsh" src"pure.zsh"
 zinit light sindresorhus/pure
 # Load the zsh-syntax-highlighting plugin.
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+# Load the zsh-autosuggestions plugin.
+zinit light zsh-users/zsh-autosuggestions
