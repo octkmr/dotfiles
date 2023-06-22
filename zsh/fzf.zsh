1d34ea9e45fdf64d26f0b6b55b4153854a11bbe9
octkmr <octkmr@gmail.com>
2023-06-22 22:52:34 +0900

    fzf.zsh作成

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -5,3 +5,29 @@ fi
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+
+fcd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+fbr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+fcat() {
+  find * -type f | fzf --layout=reverse --preview "bat --color=always {}"
+}
+
+fv() {
+  local files
+  files=$(fzf-tmux -d 40% --multi --preview-window=right:70% \
+    --preview 'bat --color=always --style=header,grid --line-range :500 {}') &&
+  nvim ${files}
+}
